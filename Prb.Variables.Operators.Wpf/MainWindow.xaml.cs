d4456e59773e9397e8dd0c76df8083ffeb9a1f5b
Herman De Beukelaer <herman.de.beukelaer@howest.be>
2021-09-21 09:59:48 +0200

    Translate code

--- Prb.Variables.Operators.Wpf/MainWindow.xaml.cs
+++ Prb.Variables.Operators.Wpf/MainWindow.xaml.cs
@@ -27,42 +27,43 @@ namespace Prb.WiskOperatoren.WPF
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            lstBewerkingen.Items.Add("Optelling: 29 + 7 = " + (29 + 7));
-            lstBewerkingen.Items.Add("Aftrekking: 29 - 7 = " + (29 - 7));
-            lstBewerkingen.Items.Add("Vermenigvuldiging: 29 * 7 = " + (29 * 7));
-            lstBewerkingen.Items.Add("Deling: 29 / 7 = " + (29F / 7F));
-            lstBewerkingen.Items.Add("Modulo : 29 % 7 = " + (29 % 7));
-            int getal = 0;
-            lstBewerkingen.Items.Add("Waarde van getal: " + getal);
-            lstBewerkingen.Items.Add("Verhogen met 1 : ++getal = " + ++getal);
-            lstBewerkingen.Items.Add("Verlagen met 1 : --getal = " + --getal);
-            lstBewerkingen.Items.Add("2 tot de 3de macht: " + Math.Pow(2, 3));
-            lstBewerkingen.Items.Add("De vierkantswortel van 16: " + Math.Sqrt(16));
+            lstOperations.Items.Add("Optelling: 29 + 7 = " + (29 + 7));
+            lstOperations.Items.Add("Aftrekking: 29 - 7 = " + (29 - 7));
+            lstOperations.Items.Add("Vermenigvuldiging: 29 * 7 = " + (29 * 7));
+            lstOperations.Items.Add("Deling: 29 / 7 = " + (29F / 7F));
+            lstOperations.Items.Add("Modulo : 29 % 7 = " + (29 % 7));
 
-            int aantalLeerlingen;
-            aantalLeerlingen = 10;
+            int number = 0;
+            lstOperations.Items.Add("Waarde van getal: " + number);
+            lstOperations.Items.Add("Verhogen met 1 : ++getal = " + ++number);
+            lstOperations.Items.Add("Verlagen met 1 : --getal = " + --number);
+            lstOperations.Items.Add("2 tot de 3de macht: " + Math.Pow(2, 3));
+            lstOperations.Items.Add("De vierkantswortel van 16: " + Math.Sqrt(16));
 
-            aantalLeerlingen = 10 + 5;
+            int numberOfStudents;
+            numberOfStudents = 10;
 
-            aantalLeerlingen = aantalLeerlingen + 8;
-            aantalLeerlingen = aantalLeerlingen - 2;
-            aantalLeerlingen = aantalLeerlingen * 3;
-            aantalLeerlingen = aantalLeerlingen / 3;
-            aantalLeerlingen = aantalLeerlingen % 4;
+            numberOfStudents = 10 + 5;
 
-            aantalLeerlingen += 8;
-            aantalLeerlingen -= 2;
-            aantalLeerlingen *= 3;
-            aantalLeerlingen /= 3;
-            aantalLeerlingen %= 4;
+            numberOfStudents = numberOfStudents + 8;
+            numberOfStudents = numberOfStudents - 2;
+            numberOfStudents = numberOfStudents * 3;
+            numberOfStudents = numberOfStudents / 3;
+            numberOfStudents = numberOfStudents % 4;
 
-            lstBewerkingen.Items.Add("Aantal leerlingen is: " + aantalLeerlingen);
+            numberOfStudents += 8;
+            numberOfStudents -= 2;
+            numberOfStudents *= 3;
+            numberOfStudents /= 3;
+            numberOfStudents %= 4;
 
-            string naam = "Kenji";
-            string voornaam = "Minogue";
-            naam += " " + voornaam;
+            lstOperations.Items.Add("Aantal leerlingen is: " + numberOfStudents);
 
-            lstBewerkingen.Items.Add("De naam is: " + naam);
+            string name = "Kenji";
+            string lastName = "Minogue";
+            name += " " + lastName;
+
+            lstOperations.Items.Add("De naam is: " + name);
         }
     }
 }
